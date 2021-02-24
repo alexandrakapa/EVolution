8f78711a2bfd7eb26cd26f7c32574aceb6e59749
Stamatis Alexandropoulos <stamatisalex7@gmail.com>
2021-02-24 22:53:19 +0200

    sign in page

--- front-end/stamatis/src/components/SignIn/index.js
+++ front-end/stamatis/src/components/SignIn/index.js
@@ -1,7 +1,87 @@
-import React from 'react';
-const SignIn = () => (
-<div>
-<h1>App</h1>
-</div>
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
+
+import * as ROUTES from '../../constants/routes';
+//const ->unchanged
+const SignInPage = () => (
+  <div>
+    <h1>SignIn</h1>
+    <SignInForm />
+  </div>
 );
-export default SignIn;
+
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+  error: null,
+};
+//component -> html object
+class SignInFormBase extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...INITIAL_STATE };
+  }
+
+  onSubmit = event => {
+    const { email, password } = this.state;
+    this.props.history.push(ROUTES.HOME);
+    // this.props.firebase
+    //   .doSignInWithEmailAndPassword(email, password)
+    //   .then(() => {
+    //     this.setState({ ...INITIAL_STATE });
+    //     this.props.history.push(ROUTES.HOME);
+    //   })
+    //   .catch(error => {
+    //     this.setState({ error });
+    //   });
+
+    event.preventDefault();
+  };
+
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+//render -> φορτώνει αντικείμενα
+  render() {
+    const { email, password, error } = this.state;
+
+    const isInvalid = password === '' || email === '';
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          name="email"
+          value={email}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Email Address"
+        />
+        <input
+          name="password"
+          value={password}
+          onChange={this.onChange}
+          type="password"
+          placeholder="Password"
+        />
+        <button disabled={isInvalid} type="submit">
+          Sign In
+        </button>
+
+        {error && <p>{error.message}</p>}
+      </form>
+    );
+  }
+}
+
+//router -> βιβλιοθήκη που κάνει τα routes δλδ συνδεέι τα links μεταξύ τους
+// H SignInFormBase παίρνει απο το withRouter
+//compose -> συνδεση συναρτησεων της withRouter με το SignInFormBase
+//ενισχυω τη δευτερη παρενθεση με οτι εχει η πρωτη παρενθεση
+const SignInForm = compose(
+  withRouter,
+)(SignInFormBase);
+
+export default SignInPage;
+
+//export { SignInForm };
