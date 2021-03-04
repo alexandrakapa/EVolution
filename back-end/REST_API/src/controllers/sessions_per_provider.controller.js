69d1a05c329ede2f2c474b694864a71074d02021
Stamatis Alexandropoulos <stamatisalex7@gmail.com>
2021-03-04 18:45:22 +0200

    Provider Per District

--- back-end/REST_API/src/controllers/sessions_per_provider.controller.js
+++ back-end/REST_API/src/controllers/sessions_per_provider.controller.js
@@ -1,30 +1,126 @@
  const SessionModel=require('../models/sessions_per_provider.model');
 
-   exports.getProvider=(req, res) => {
-   	if ( ((req.params.yyyymmdd_from.match(/[0-2][0-9][0-9][0-9][0][1-9][0][1-9]/)) || (req.params.yyyymmdd_from.match(/[0-2][0-9][0-9][0-9][0][1-9][1-2][0-9]/)) || (req.params.yyyymmdd_from.match(/[0-2][0-9][0-9][0-9][0][1-9][3][0-1]/)) || (req.params.yyyymmdd_from.match(/[0-2][0-9][0-9][0-9][1][0-2][0][1-9]/)) || (req.params.yyyymmdd_from.match(/[0-2][0-9][0-9][0-9][1][0-2][1-2][0-9]/))|| (req.params.yyyymmdd_from.match(/[0-2][0-9][0-9][0-9][1][0-2][3][1-2]/)) )&&
- 	     ((req.params.yyyymmdd_to.match(/[0-2][0-9][0-9][0-9][0][1-9][0][1-9]/)) || (req.params.yyyymmdd_to.match(/[0-2][0-9][0-9][0-9][0][1-9][1-2][0-9]/)) || (req.params.yyyymmdd_to.match(/[0-2][0-9][0-9][0-9][0][1-9][3][0-1]/)) || (req.params.yyyymmdd_to.match(/[0-2][0-9][0-9][0-9][1][0-2][0][1-9]/)) || (req.params.yyyymmdd_to.match(/[0-2][0-9][0-9][0-9][1][0-2][1-2][0-9]/))|| (req.params.yyyymmdd_to.match(/[0-2][0-9][0-9][0-9][1][0-2][3][1-2]/)))){
 
- 	 	SessionModel.getProviderByID(req, (err, data) => {
- 	 		if (err) {
- 	 			res.send(err);
- 	 			return;
- 	 		}
- 	 		else if (data.length){
- 	 			res.send(data);
- 	 			return;
- 	 		}
- 	 		else {
- 	 			res.statusMessage='No data';
- 	 			res.status(402).send('No provider with this ID.');
- 	 			return;
- 	 		}
+ check_day = (y, m, d) => {
+    const months30 = ['04', '06', '09', '11'];
+    const months31 = ['01', '03', '05', '07', '08', '10', '12'];
 
- 	 	});
- 	 }
- 	 else{
- 	 	console.log('Invalid');
- 	 	res.statusMessage='Bad request';
- 	 	res.status(400).send('Bad request: A date you entered is not a valid data.');
- 	 	return;
- 	 }
-  };
+    if (m == 2) {
+        if (y % 4 == 0) {
+            if (d > 29) {
+                return true;
+            }
+        }
+        else if (d > 28) {
+            return true;
+        }
+    }
+
+    else if (months30.includes(m)) {
+        if (d > 30) {
+            return true;
+        }
+    }
+
+    else if (months31.includes(m)) {
+        if (d >31) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+  exports.getProvider=(req, res) => {
+
+ 	//check if any of the variables given is empty
+  	if (Object.keys(req.params).length!=3){
+  		res.statusMessage = 'Bad Request';
+        res.status(400).send('Bad Request : Empty Required Field');
+        return;
+  	}
+
+    const ID = req.params.providerID;
+    //check if provider ID length is valid based on our database's corresponding attribute's type
+    if (ID.length > 255+255) {
+        res.statusMessage = 'Bad Request';
+        res.status(400).send('Bad Request : Invalid Provider ID');
+        return;
+    }
+
+
+    //check if date length is indeed 8 , because we want format yyyymmdd
+    if (req.params.yyyymmdd_from.length != 8 || req.params.yyyymmdd_to.length != 8) {
+        res.statusMessage = 'Bad Request';
+        res.status(400).send('Bad Request : Invalid Date Format');
+        return;
+    }
+
+    //Start date check
+    const from_year = (req.params.yyyymmdd_from).substring(0,4);
+    const to_year =  (req.params.yyyymmdd_to).substring(0,4);
+    const from_month = (req.params.yyyymmdd_from).substring(4,6);
+    const to_month = (req.params.yyyymmdd_to).substring(4,6);
+    const from_day = (req.params.yyyymmdd_from).substring(6,8);
+    const to_day = (req.params.yyyymmdd_to).substring(6,8);
+
+    //General easy checks: everything must be >= 1 and months must be < 12
+    if (from_year < 1 || to_year < 1 || from_month < 1 || from_month > 12 || to_month < 1 || to_month > 12 || from_day < 1 || to_day < 1 ) {
+        res.statusMessage = 'Bad Request';
+        res.status(400).send('Bad Request : Invalid Dates');
+        return;
+    }
+
+    //check if day < (right int)
+    if (check_day(from_year, from_month, from_day) ) {
+        res.statusMessage = 'Bad Request';
+        res.status(400).send('Bad Request : Invalid Dates');
+        return;
+    }
+    if (check_day(to_year, to_month, to_day) ) {
+        res.statusMessage = 'Bad Request';
+        res.status(400).send('Bad Request : Invalid Dates');
+        return;
+    }
+
+    //Check if from_date < to_date
+    if (from_year > to_year) {
+        res.statusMessage = 'Bad Request';
+        res.status(400).send('Bad Request : Starting Period Date must precede Ending Period Date');
+        return;
+    }
+    else if (to_year == from_year) {
+        if (from_month > to_month) {
+            res.statusMessage = 'Bad Request';
+            res.status(400).send('Bad Request : FromMonth > ToMonth');
+            return;
+        }
+        else if (to_month == from_month) {
+            if (from_day > to_day) {
+                res.statusMessage = 'Bad Request';
+                res.status(400).send('Bad Request : FromDay > ToDay');
+                return;
+            }
+        }
+    }
+
+	 	SessionModel.getProviderByID(req, (err, data) => {
+	 		if (err) {
+	 			res.send(err);
+	 			return;
+	 		}
+	 		else if (data.length){
+	 			res.send(data);
+	 			return;
+	 		}
+	 		else {
+	 			//console.log(res);
+	 			res.statusMessage='No data';
+	 			res.status(402).send('No Charging Point with this ID.');
+	 			return;
+	 		}
+
+	 	});
+
+
+ };
--- back-end/REST_API/src/models/sessions_per_provider_per_district.model.js
+++ back-end/REST_API/src/models/sessions_per_provider_per_district.model.js
@@ -18,20 +18,20 @@ const parsedate= function(){
 }
 
 
-Manufacturer.getSessionsbyManID = async (req, result) => {
+Supplier.getSessionsbyManID = async (req, result) => {
 
 	//console.log(parsedate());
 	let arr=new Array();
-	let SupplierID=(req.params.supplierID);
+	let supplierID=(req.params.supplierID);
+	let region=(req.params.region);
 	let periodfrom=((req.params.yyyymmdd_from).substring(0,4)).concat('-',(req.params.yyyymmdd_from).substring(4,6),'-',(req.params.yyyymmdd_from).substring(6,8));
 	let periodto=((req.params.yyyymmdd_to).substring(0,4)).concat('-',(req.params.yyyymmdd_from).substring(4,6),'-',(req.params.yyyymmdd_from).substring(6,8));
-	let region=(req.params.region);
-	console.log('SupplierID ',SupplierID);
+	console.log('SupplierID ',supplierID);
 	console.log('region',region);
 	//console.log(periodfrom);
-	dbConn.query(`SELECT Energy_Supplier.ID,Energy_Supplier.company_name,SUM(Charing.kWh_delivered) as Total_Energy_Delivered
+	dbConn.query(`SELECT Energy_Supplier.ID as SupplierID ,Energy_Supplier.company_name as SupplierName ,SUM(Charging.kWh_delivered) as Total_Energy_Delivered
 	FROM Space,Energy_Supplier,Station,Charging
-	WHERE Energy_Supplier.ID='${providerid}' and Space.StationID=Station.ID and SUBSTRING(concat(Station.PostalCode,''), 1, 2)='${region}' and Charging.SpaceStationID=Station.ID and Charging.supplierID=Energy_Supplier.ID and DATE(STR_TO_DATE(Charging.the_date, '%c/%e/%Y %H:%i'))>=(SELECT DATE(${req.params.yyyymmdd_from}) FROM dual) AND DATE(STR_TO_DATE(Charging.the_date, '%c/%e/%Y'))<=(SELECT DATE(${req.params.yyyymmdd_to}) FROM dual)`
+	WHERE Energy_Supplier.ID='${supplierID}' and Space.StationID=Station.ID and SUBSTRING(concat(Station.PostalCode,''), 1, 2)='${region}' and Charging.SpaceStationID=Station.ID and Charging.supplierID=Energy_Supplier.ID AND DATE(STR_TO_DATE(Charging.the_date, '%c/%e/%Y %H:%i'))>=(SELECT DATE(${req.params.yyyymmdd_from}) FROM dual) AND DATE(STR_TO_DATE(Charging.the_date, '%c/%e/%Y'))<=(SELECT DATE(${req.params.yyyymmdd_to}) FROM dual)`
 	, (err, res) =>
 	{
 		if (err) {
@@ -39,18 +39,19 @@ Manufacturer.getSessionsbyManID = async (req, result) => {
 		    result(err, null);
 		    return;
 		    }
-		if (res.length){
-			//console.log(res[0]);
+				console.log(res[0]['SupplierID']);
+		if (res[0]['SupplierID']!=null){
+			console.log(res[0]['SupplierID']);
 			console.log('Found Supplier.')
 			//arr.push(res);
 			arr.push({SupplierID: res[0]['SupplierID']});
 			arr.push({SupplierName: res[0]['SupplierName']});
-			arr.push({RequestTimestamp: parsedate()})
 			arr.push({PeriodFrom: periodfrom});
 			arr.push({PeriodTo: periodto});
-			arr.push({TotalEnergyDelivered: res[0]['Total_Energy_Delivered']})
+			arr.push({Region: region});
+			arr.push({TotalEnergyDelivered: res[0]['Total_Energy_Delivered']});
 			//Session.getter(req, arr, result);
-			//result(null, arr);
+			result(null, arr);
 			return;
 
 		}
@@ -62,90 +63,5 @@ Manufacturer.getSessionsbyManID = async (req, result) => {
 
 	});
 }
-
-
-Session.getter= async ( req, arr, result ) => {
-	//Point.getPointByID(req, result);
-	let sessionlist=new Array();
-	let SupplierID=(req.params.supplierID);
-	let region=(req.params.region);
-	console.log('SupplierID ',SupplierID);
-	console.log('region',region);
-	// dbConn.query(`
-	// 	SELECT SUM(kWh_delivered)
-	// 	FROM Charging
-	// 	WHERE DATE(STR_TO_DATE(Charging.the_date, '%c/%e/%Y %H:%i'))>=(SELECT DATE(${req.params.yyyymmdd_from}) FROM dual) AND DATE(STR_TO_DATE(Charging.the_date, '%c/%e/%Y'))<=(SELECT DATE(${req.params.yyyymmdd_to}) FROM dual)
-	//
-	// 	`
-
-
-	dbConn.query(`
-		SELECT (@rank := @rank+1) as SessionIndex, F.SessionID, F.CarID, F.Model, F.UsableBatterySize, F.StartedOn, F.FinishedOn,  F.Protocol, F.EnergyDelivered, F.BatteryPercentBegin, F.BatteryPercentEnd, F.kmTotal, F.kmBetweenCharges, F.StationAddress
-		FROM (
-
-
-
-		SELECT A.SessionID, A.CarID, A.Model, A.UsableBatterySize, A.StartedOn, A.FinishedOn,  A.Protocol, A.EnergyDelivered, A.BatteryPercentBegin, A.BatteryPercentEnd, A.kmTotal, A.kmBetweenCharges, G.StationAddress, G.PostalCode
-		FROM
-
-
-		( SELECT * FROM
-
-
-		(
-		(SELECT ID as SupplierID
-		FROM Car
-		WHERE Car.Car_ManufacturerID=${ManufacturerID}
-		) as B
-		INNER JOIN
-		( SELECT Charging.ID as SessionID, SUBSTRING(CONCAT(STR_TO_DATE(SUBSTRING(Charging.connection_time,6,11), "%d %b %Y"), SUBSTRING(Charging.connection_time,17,17) ), 1, 19) as StartedOn, SUBSTRING(CONCAT(STR_TO_DATE(SUBSTRING(Charging.disconnection_time,6,11), "%d %b %Y"), SUBSTRING(Charging.disconnection_time,17,17) ), 1, 19) as FinishedOn,
-		Charging.kWh_delivered as EnergyDelivered, Charging.battery_percent_begin as BatteryPercentBegin, Charging.battery_percent_end as BatteryPercentEnd, Charging.km_total as kmTotal,
-		Charging.km_between_charges as kmBetweenCharges, Charging.protocol as Protocol, Charging.SpaceStationID as StationID, Charging.CarID as CarID
-		 FROM Charging
-		WHERE
-		DATE(STR_TO_DATE(Charging.the_date, '%c/%e/%Y %H:%i'))>=(SELECT DATE(${req.params.yyyymmdd_from}) FROM dual) AND DATE(STR_TO_DATE(Charging.the_date, '%c/%e/%Y'))<=(SELECT DATE(${req.params.yyyymmdd_to}) FROM dual)
-		) as T
-		ON T.CarID=B.ID
-		)
-		) as A
-		LEFT JOIN
-		(SELECT Station.ID as StationID, CONCAT(Station.address_info,', ', Station.postalcode, ', ', Station.country) as StationAddress, Station.postalcode as PostalCode
-		FROM Station
-		) as G
-		on A.StationID=G.StationID
-		) as F, (SELECT @rank:=0) r
-		WHERE SUBSTRING(concat(F.PostalCode,''), 1, 2)='${region}'
-		ORDER BY F.StartedOn DESC`, (err,res) =>
-		{
-
-		 //console.log(res);
-		 if (err) {
-		    console.log("error: ", err);
-		    result(err, null);
-		    return;
-		    }
-
-		 if (res.length) {
-		 	arr.push({NumberOfChargingSessions: res.length});
-		    console.log("Found Charging Sessions.");
-		    //result(null, res);
-		    arr.push(res);
-		    //console.log(arr[0]['NumberOfChargingSessions']);
-		    //console.log(arr[1][0]['SessionIndex']);
-		    result(null, arr);
-		    return;
-
-		    }
-
-		    // not found
-		    console.log('No Energy Delivered for these dates and this region.')
-		    arr.push({NumberOfChargingSessions: 0});
-		    arr.push([]);
-		    result(null, arr);
-		    return;
-		 });
-};
-
-
 //module.exports=Session;
-module.exports=Manufacturer;
+module.exports=Supplier;
