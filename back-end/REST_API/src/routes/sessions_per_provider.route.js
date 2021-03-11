dc7306c4a8fbfc0b2dd36a2647505e9eb94c0c48
Stamatis Alexandropoulos <64039583+stamatisalex@users.noreply.github.com>
2021-03-11 14:02:51 +0200

    Sessions Per Provider

--- back-end/REST_API/src/routes/sessions_per_provider.route.js
+++ back-end/REST_API/src/routes/sessions_per_provider.route.js
@@ -5,7 +5,7 @@ const sessionController = require('../controllers/sessions_per_provider.controll
 
 // router.get('/:providerID/:yyyymmdd_from/:yyyymmdd_to', sessionController.getProvider);
 const middle_check = require('../authentication/auth.js');
-router.get('/:supplierID/:station/:yyyymmdd_from/:yyyymmdd_to',function(req, res){
+router.get('/:supplierID/:yyyymmdd_from/:yyyymmdd_to',function(req, res){
   middle_check.findByToken(req,res,sessionController.getProvider,0)
 });
 module.exports = router;
