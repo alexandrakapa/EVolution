438a2ed8556c9cd148503880958884c1154dff52
alexandrakapa <aleksandrakapa@hotmail.com>
2021-03-13 17:01:45 +0200

    fixed csv to json

--- back-end/REST_API/src/models/chargingData.model.js
+++ back-end/REST_API/src/models/chargingData.model.js
@@ -1,4 +1,5 @@
 var dbConn  = require('../../config/db.config');
+const converter = require('json-2-csv');
 
 var Charging = function(charging) {
   this.ID = charging.ID
@@ -22,40 +23,75 @@ var Charging = function(charging) {
 }
 
 
-
-Charging.getChargingData = (chargingID,result) => {
+Charging.getChargingData = (chargingID,format,result) => {
   dbConn.query('SELECT * FROM Charging WHERE ID = ?' , chargingID, (err, res)=>{
       if(err){
           console.log('Error while fetching charging by ID', err);
           result(null,err);
       }else{
-          result(null,res);
+        if (format=='csv'){
+          console.log("found it")
+          var tocsv=res
+
+        converter.json2csv(tocsv, (err, csv) =>{
+          if (err) {
+            result(err,null)
+          }
+
+          else {
+            //result.attachment('results.csv').send(csv)
+            result(null,csv)
+          }
+        },{emptyFieldValue  : ''})
+  }
+  else {
+      result(null,res)
+    }
       }
   });
 }
 
-Charging.getChargingDataByOwner = (username,vehicleID,result) => {
-  dbConn.query('SELECT ID,kWh_delivered,protocol,battery_percent_begin,battery_percent_end, charging_price FROM Charging WHERE Car_Ownerusername = ? AND CarID = ? LIMIT 1' , [username,vehicleID], (err, res)=>{
+
+Charging.getChargingDataByOwner = (username,vehicleID,format,result) => {
+  dbConn.query('SELECT ID,kWh_delivered,protocol,battery_percent_begin,battery_percent_end,charging_price  FROM Charging WHERE Car_Ownerusername = ? AND CarID = ? LIMIT 1' , [username,vehicleID], (err, res)=>{
       if(err){
           console.log('Error while fetching charging by owner and vehicle', err);
           result(null,err);
       }else{
-          result(null,res);
+        if (format=='csv'){
+          console.log("found it")
+          var tocsv=res
+
+        converter.json2csv(tocsv, (err, csv) =>{
+          if (err) {
+            result(err,null)
+          }
+
+          else {
+            //result.attachment('results.csv').send(csv)
+            result(null,csv)
+          }
+        },{emptyFieldValue  : ''})
+  }
+  else {
+      result(null,res)
+    }
       }
+
   });
 }
 
 
-Charging.getChargingCost = (chargingID,result) => {
-  dbConn.query('SELECT kWh_delivered*charging_price as total_cost FROM `Charging` WHERE ID = ?' , chargingID, (err, res)=>{
-      if(err){
-          console.log('Error while fetching charging cost by ID', err);
-          result(null,err);
-      }else{
-          result(null,res);
-      }
-  });
-}
+// Charging.getChargingCost = (chargingID,result) => {
+//   dbConn.query('SELECT kWh_delivered*charging_price as total_cost FROM `Charging` WHERE ID = ?' , chargingID, (err, res)=>{
+//       if(err){
+//           console.log('Error while fetching charging cost by ID', err);
+//           result(null,err);
+//       }else{
+//           result(null,res);
+//       }
+//   });
+// }
 
 
 module.exports = Charging;
